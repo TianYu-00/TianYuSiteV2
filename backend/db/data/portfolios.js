56f85ca956d6479e58027354d4420a06e7b54147
TianYu-00 <imniyeye@gmail.com>
2024-07-19 04:08:02 +0100

    update portfolios

--- backend/db/data/portfolios.js
+++ backend/db/data/portfolios.js
@@ -1,32 +1,222 @@
 module.exports = [
   {
     portfolio_name: "Nc News API",
-    portfolio_short_description: "Random Short Description",
+    portfolio_short_description:
+      "An app developed by team Whalers on the Moon to allow users to generate suggestions for date night, whether that is with your partner, friend, or as a solo outing.",
     portfolio_start_date: "May 28 2024",
     portfolio_project_type: "Northcoders Project",
-    portfolio_description: "Random Long Description",
+    portfolio_description:
+      "This project involves building an API designed to provide programmatic access to application data. The goal is to simulate the development of a real-world backend service (similar to Reddit) that delivers this data to the frontend architecture.",
     portfolio_youtube: "",
     portfolio_github: "https://github.com/TianYu-00/be-nc-news",
     portfolio_demo: "https://nc-portfolio-1.onrender.com/api",
   },
   {
     portfolio_name: "Nc News",
-    portfolio_short_description: "Random Short Description",
+    portfolio_short_description: "A social news aggregation, web content rating and discussion website.",
     portfolio_start_date: "June 17 2024",
     portfolio_project_type: "Northcoders Project",
-    portfolio_description: "Random Long Description",
+    portfolio_description:
+      "This project involves creating Northcoders News - a social news aggregation, web content rating and discussion website. Northcoders News has articles which are divided into topics, and each article has user-curated ratings from upvotes and downvotes using the API. Users can also add comments about an article.",
     portfolio_youtube: "https://www.youtube.com/watch?v=UvF9yI6z3YE",
     portfolio_github: "https://github.com/TianYu-00/fe-nc-news",
     portfolio_demo: "https://fe-nc-news-tian.netlify.app/",
   },
   {
     portfolio_name: "Whimsy Date",
-    portfolio_short_description: "Random Short Description",
+    portfolio_short_description:
+      "An API to provide programmatic access to application data, simulating a real-world backend service (like Reddit) for frontend architecture.",
     portfolio_start_date: "July 01 2024",
     portfolio_project_type: "Northcoders Project",
-    portfolio_description: "Random Long Description",
+    portfolio_description:
+      "Whimsy Date is an app developed by Whalers on the Moon to allow users to generate suggestions for date night, whether that is with your partner, friend, or as a solo outing.",
     portfolio_youtube: "https://www.youtube.com/watch?v=3c1I65A9X9w",
     portfolio_github: "https://github.com/TianYu-00/WhimsyDate",
     portfolio_demo: "",
+  },
+  {
+    portfolio_name: "2D Top Down Shooter",
+    portfolio_short_description:
+      "2D Top down shooter game where the player has the freedom to select their unique abilities. The primary objective is survival, where the player faces successive waves of enemies.",
+    portfolio_start_date: "SEPTEMBER 21 2023",
+    portfolio_project_type: "Personal Project",
+    portfolio_description: `The 2D Bullet Hell game is a work in progress that I'm actively developing in my spare time. The current aim
+                of the game is to survive as long as you can. After reaching a certain score by killing mobs they are
+                provided with the opportunity to select one ability from a list of abilities displayed on screen.
+                
+                I'm aiming for a minimalistic design, featuring square-shaped player characters and mobs distinguished by
+                different colours. To enhance the visual appeal, I've incorporated blooming effects and camera shakes,
+                lending a futuristic and engaging feel. Despite the minimalism, I plan to introduce additional effects and
+                animations for both players and mobs in the future.
+
+                Currently, the map is restricted to the size of the camera view. I intend to maintain this confined space,
+                creating a survival challenge for the player, unlike other games where exploration involves traversing larger
+                maps. However, this design choice presents potential balancing issues. For instance, a small map size may
+                lead to challenges such as an abundance of mobs causing the player to rapidly lose health, thus increasing
+                the game's difficulty due to limited manoeuvring space.
+                `,
+    portfolio_youtube: "https://www.youtube.com/watch?v=VZXbzsf5cXU",
+    portfolio_github: "https://github.com/TianYu-00/Unity-2DBulletHell",
+    portfolio_demo: "",
+  },
+  {
+    portfolio_name: "2.5D Clone Platformer",
+    portfolio_short_description:
+      "A small prototype project suggested by ChatGPT when asked. The primary objective is to reach the finish point.",
+    portfolio_start_date: "MARCH 08 2023",
+    portfolio_project_type: "Personal Project",
+    portfolio_description: `The 2.5D Clone Platformer is a compact project I developed during my free time. The concept for this game
+                emerged from a suggestion by ChatGPT when I sought a smaller-scale project idea. Throughout the development
+                phase, I focused on rapidly prototyping the features proposed by ChatGPT, specifically centred around
+                integrating a clone within the platformer game. This clone serves to assist the player in accessing
+                unreachable areas and engaging with triggers that would otherwise be beyond the player's reach.
+                
+                Since this project was primarily focused on quickly establishing the mechanics, I didn't dedicate extensive
+                effort to the level's design or embellishments. Nevertheless, I incorporated a level-completion animation and
+                enhanced the player's appearance by adding glasses instead of a basic cube model.
+
+                The player can summon a clone by pressing a designated button. Once the clone is summoned, the player becomes
+                immobilized while the clone can utilize the player as a platform to access areas otherwise unreachable.
+                Incorporating this feature with a well-crafted level design has the potential to create an engaging and
+                challenging gaming experience, offering an enjoyable gameplay dynamic.
+                `,
+    portfolio_youtube: "https://youtu.be/1936MOAyG1g",
+    portfolio_github:
+      "https://github.com/TianYu-00/Unity-ClonePuzzlePlatformer/tree/7973e7ce93a9f01a1fdc28ecaee82eff9daf6fa0/Clone%20Puzzle%20Platformer",
+    portfolio_demo: "",
+  },
+  {
+    portfolio_name: "3D Third Person Shooter",
+    portfolio_short_description:
+      "This is a demonstration project showcasing the mechanics of walking, aiming, and shooting.",
+    portfolio_start_date: "JULY 3 2023",
+    portfolio_project_type: "Personal Project",
+    portfolio_description: `The 3D Third Person Shooter stands as my prototype project showcasing shooting mechanics, bone-to-weapon
+                rigging, movement animations, and camera aiming effects, encapsulating the foundational elements of any
+                third-person shooter. As a prototype, I focused solely on understanding these specific features without
+                delving into map design or extensive animation effects—just the core basics.
+                
+                Exploring and mastering these aspects was a fascinating learning experience, as it paves the way for
+                leveraging these skills in my future game development endeavours. This newfound understanding opens up a
+                realm of possibilities for integrating these features into upcoming projects.
+                `,
+    portfolio_youtube: "https://youtu.be/sYDjvAt8WiA",
+    portfolio_github: "",
+    portfolio_demo: "",
+  },
+  {
+    portfolio_name: "2D Light&Dark Theme Platformer",
+    portfolio_short_description:
+      "platformer game set in a 2D environment where the player has the freedom to place blocks in place during their movement. The primary objective is to reach the finish point while bypassing obstacles.",
+    portfolio_start_date: "NOVEMBER 16 2020",
+    portfolio_project_type: "Personal Project",
+    portfolio_description: `This 2D project was developed as part of my university game jam coursework. Over six weeks, we were presented
+                with a new game theme each week, and I meticulously managed tasks and game concepts to ensure their
+                feasibility within the given time frame. Upon completing the core mechanics of each game, I dedicated
+                additional time to refining and expanding the gameplay, including the creation of extra levels.
+
+                The theme for my third week of the game jam was "Light and Dark." During the planning phase, I generated two
+                distinct game ideas. The initial concept involved a platformer with contrasting dark and light-coloured
+                platforms. The second idea centred around a 2D game where the player wielded a torch to illuminate their
+                path, introducing a challenge by leaving areas behind and to the sides of the player in darkness. Ultimately,
+                I chose my first idea at last as I had more mechanic ideas for the first game during the time.
+
+                In refining the first game concept, I conceived an innovative approach to differentiate it from conventional
+                platformers. Instead of pre-designed platforms, I allowed users to create their own, adding a unique twist to
+                the gameplay. To maintain a level of challenge, I incorporated obstacles that players needed to navigate
+                around while creating their platforms.
+                `,
+    portfolio_youtube: "https://youtu.be/eNoeY-UQapw",
+    portfolio_github:
+      "https://github.com/TianYu-00/Unity_GameAWeek/tree/77fe1f8f5b4407798218c718fb4c3ee8180507a4/SubmitGame3%20-%20Light%26Dark/Light%26Dark",
+    portfolio_demo:
+      "https://github.com/TianYu-00/Unity_GameAWeek/tree/77fe1f8f5b4407798218c718fb4c3ee8180507a4/SubmitGame3%20-%20Light%26Dark/Game3Build",
+  },
+  {
+    portfolio_name: "3D Minigame",
+    portfolio_short_description:
+      "Platformer game set in a 3D environment where the player has to get to the right color block before time runs out.",
+    portfolio_start_date: "AUGUST 5 2020",
+    portfolio_project_type: "University Project",
+    portfolio_description: `This 3D project was developed as part of my university game jam coursework. Over six weeks, we were presented
+                with a new game theme each week, and I meticulously managed tasks and game concepts to ensure their
+                feasibility within the given time frame. Upon completing the core mechanics of each game, I dedicated
+                additional time to refining and expanding the gameplay, including the creation of extra levels.
+                
+                During the first week of the coursework game jam, the assigned theme was "Minigame." In the planning phase, I
+                quickly came up with an idea for a party-themed game where the main objective is to reach a designated
+                coloured block within a given time limit. Failing to do so will result in losing the game. The primary aim of
+                the game is to stay alive for as long as possible, trying to beat personal best scores with each play through.
+                `,
+    portfolio_youtube: "https://youtu.be/RcjLCrKqVBg",
+    portfolio_github:
+      "https://github.com/TianYu-00/Unity_GameAWeek/tree/77fe1f8f5b4407798218c718fb4c3ee8180507a4/SubmitGame1%20-%20Minigame/Source/Minigame",
+    portfolio_demo: "",
+  },
+  {
+    portfolio_name: "2D Cooperation&Competition",
+    portfolio_short_description:
+      "Street fighter type game set in a 2D environment where the player play against another player locally.",
+    portfolio_start_date: "NOVEMBER 5 2020",
+    portfolio_project_type: "University Project",
+    portfolio_description: `This 2D project was developed as part of my university game jam coursework. Over six weeks, we were presented
+                with a new game theme each week, and I meticulously managed tasks and game concepts to ensure their
+                feasibility within the given time frame. Upon completing the core mechanics of each game, I dedicated
+                additional time to refining and expanding the gameplay, including the creation of extra levels.
+                
+                During the fourth week of the game jam, we were given the theme of "Cooperation and Competition." I spent a
+                significant amount of time brainstorming ideas to create an engaging and unique game that would satisfy this
+                theme. After much consideration, I decided to develop a local two-player cooperative game where the players
+                compete against each other, similar to Street Fighter. I believed that this would be an exciting way to
+                encourage competition between the players.
+                `,
+    portfolio_youtube: "https://youtu.be/1jOz6dIc1NU",
+    portfolio_github:
+      "https://github.com/TianYu-00/Unity_GameAWeek/tree/a19eacd9de895111eb4b3f0576e51d2a9c95d926/SubmitGame4%20-%20Cooperation%26Competition/Cooperation%26Competition",
+    portfolio_demo:
+      "https://github.com/TianYu-00/Unity_GameAWeek/tree/a19eacd9de895111eb4b3f0576e51d2a9c95d926/SubmitGame4%20-%20Cooperation%26Competition/Build",
+  },
+  {
+    portfolio_name: "2D Collectable Card Game",
+    portfolio_short_description: "x",
+    portfolio_start_date: "MAY 16 2022",
+    portfolio_project_type: "University Project",
+    portfolio_description: `During my time at university, I worked on developing a 2D project for my coursework that had a Chinese CCG
+                theme. I was inspired to choose this theme as there were very few such themes in the field. My understanding
+                of Chinese culture and background helped me to create various elements such as cards, characters, and audio
+                components.
+
+                In my exploration of the Three Kingdoms concept, I am incorporating characters from this historical period,
+                aligning their abilities, mana, attack, and defence closely with their real-life counterparts. For instance,
+                drawing inspiration from Hua Tuo, a renowned surgeon during the Three Kingdoms era, I depict him as a healing
+                character, reflecting his historical role. Considering his age and frailty, I assign him low attack and
+                defence attributes. This approach extends to other characters, ensuring a faithful representation for a more
+                immersive character environment.
+
+                Each card in the project features essential details such as mana cost, attack, defence, name, description,
+                and character image. Similarly, for enemy characters, the display includes their name, attack, health, and an
+                accompanying character image. This meticulous alignment strives to create a realistic and engaging portrayal
+                of the characters within the game environment.
+
+                The game mechanics are designed as a single-player Rogue-like Collectible Card Game (CCG). Players engage by
+                strategically dragging and dropping cards onto enemy containers to initiate attacks. Within each round, a
+                30-second timer challenges players to strategically place their cards before the enemy's counterattack.
+                Subsequently, a new timer prompts players to interact with buttons, determining the duration of their actions
+                before initiating the next round.
+
+                Mana plays a crucial role in card deployment. To accumulate mana, players must click on randomly generated
+                blue buttons that appear on screen each round, adding an element of luck to the resource management aspect.
+                This introduces a need for players to plan their attacks, accumulate mana strategically, and unleash powerful
+                combos. Success hinges on effective mana and combo management, as players aim to eliminate enemies using the
+                cards in their hands.
+
+                The game unfolds in waves, with each wave presenting a new set of challenges as players progress through
+                enemy spawns. Strategic planning, efficient mana utilization, and adept combo execution become essential for
+                achieving the goal of defeating enemies and progressing through successive waves.
+                `,
+    portfolio_youtube: "https://youtu.be/-KPOI8hnoa0",
+    portfolio_github:
+      "https://github.com/TianYu-00/Unity-2DCCG/tree/553b6824484f01b35189f400a1afcaaddb82850d/CCG2DSingle",
+    portfolio_demo: "https://github.com/TianYu-00/Unity-2DCCG/tree/553b6824484f01b35189f400a1afcaaddb82850d/Build",
   },
 ];
