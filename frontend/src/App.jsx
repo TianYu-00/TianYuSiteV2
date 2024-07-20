ac4b0d41cf788a405a4cfb6520b956f7d570b053
TianYu-00 <imniyeye@gmail.com>
2024-07-20 20:08:54 +0100

    initial commit for frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,29 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import React, { useState, useEffect } from "react";
+
+// routes
+import Header from "./pages/Header";
+import Footer from "./pages/Footer";
+import FallbackRoute from "./pages/FallBackRoute";
+import Home from "./pages/Home";
+import Portfolios from "./pages/Portfolios";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/portfolios" element={<Portfolios />} />
+          <Route path="*" element={<FallbackRoute />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/Portfolios.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Portfolios() {
+  return (
+    <div>
+      <h1>Portfolios</h1>
+    </div>
+  );
+}
