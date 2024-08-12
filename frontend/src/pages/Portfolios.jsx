3b06dea30b1f6cbcb7841780d104afbcbaae300d
TianYu-00 <imniyeye@gmail.com>
2024-08-12 19:42:40 +0100

    add showing limit and show more feature + is loading feature to make sure portfolios are fully loaded before showing the full page

--- frontend/src/pages/Portfolios.jsx
+++ frontend/src/pages/Portfolios.jsx
@@ -4,78 +4,105 @@ import { fetchPortfolios } from "../api";
 export default function Portfolios() {
   const [basePortfolios, setBasePortfolios] = useState([]);
   const [portfoliosShowing, setPortfoliosShowing] = useState([]);
+  const [allowedShowing, setAllowedShowing] = useState(3);
+  const [isLoadingPortfolios, setIsLoadingPortfolios] = useState(true);
 
   useEffect(() => {
     fetchPortfolios().then((response) => {
       setBasePortfolios(response);
-      setPortfoliosShowing(response);
+      setPortfoliosShowing(response.slice(0, allowedShowing));
+      setIsLoadingPortfolios(false);
     });
   }, []);
 
+  useEffect(() => {
+    setPortfoliosShowing(basePortfolios.slice(0, allowedShowing));
+  }, [allowedShowing]);
+
+  function onClick_ShowMore() {
+    setAllowedShowing(allowedShowing + 3);
+  }
+
+  if (isLoadingPortfolios) {
+    return <p>Loading...</p>;
+  }
+
   return (
-    <div className="relative flex flex-col items-center px-4 md:px-8 max-w-screen-xl m-auto">
-      {/* Line */}
-      <div className="hidden md:block absolute left-1/2 transform -translate-x-1/2 w-px bg-gray-500/50 glow:bg-green-500/50 h-full" />
+    <>
+      <div className="relative flex flex-col items-center px-4 md:px-8 max-w-screen-xl m-auto">
+        {/* Line */}
+        <div className="hidden md:block absolute left-1/2 transform -translate-x-1/2 w-px bg-gray-500/50 glow:bg-green-500/50 h-full" />
 
-      {portfoliosShowing.map((portfolio, index) => {
-        const formattedDate = new Date(portfolio.portfolio_start_date).toISOString().split("T")[0];
+        {portfoliosShowing.map((portfolio, index) => {
+          const formattedDate = new Date(portfolio.portfolio_start_date).toISOString().split("T")[0];
 
-        return (
-          <div
-            key={index}
-            className={`flex flex-col md:flex-row items-center w-full mb-8 ${
-              index % 2 === 0 ? "md:justify-start" : "md:justify-end"
-            }`}
-          >
-            {/* Content */}
+          return (
             <div
-              className={`relative w-full md:w-1/2 ${
-                index % 2 === 0 ? "md:text-right md:pr-8" : "md:text-left md:pl-8"
+              key={index}
+              className={`flex flex-col md:flex-row items-center w-full mb-8 ${
+                index % 2 === 0 ? "md:justify-start" : "md:justify-end"
               }`}
             >
-              <div className="p-4 rounded-lg shadow-md glow:text-green-500/100 text-copy-primary text-sm md:text-base border-2 border-border">
-                <strong className="block text-base md:text-lg">{portfolio.portfolio_name}</strong>
-
-                <p className="mt-2 text-xs md:text-sm">{portfolio.portfolio_short_description}</p>
-                <div className="overflow-hidden my-2">
-                  <img
-                    src="https://picsum.photos/seed/picsum/1920/1080"
-                    alt="Portfolio"
-                    className="w-full h-auto transition-transform duration-500 ease-in-out transform hover:scale-105"
-                  />
-                </div>
-                <div className="mb-2">
-                  {portfolio.portfolio_demo && (
-                    <a href={`${portfolio.portfolio_demo}`} className="underline underline-offset-4 mr-4">
-                      Demo
-                    </a>
-                  )}
-                  {portfolio.portfolio_github && (
-                    <a href={`${portfolio.portfolio_github}`} className="underline underline-offset-4 mr-4">
-                      Github
-                    </a>
-                  )}
-                  {portfolio.portfolio_youtube && (
-                    <a href={`${portfolio.portfolio_youtube}`} className="underline underline-offset-4 mr-4">
-                      YouTube
-                    </a>
-                  )}
-                </div>
-                <p className="text-copy-primary/35 text-sm">
-                  {portfolio.portfolio_project_type} || {formattedDate}
-                </p>
-              </div>
-
-              {/* Marker */}
+              {/* Content */}
               <div
-                className={`hidden md:block absolute top-1/2 transform -translate-y-1/2 w-4 h-4 md:w-6 md:h-6 bg-green-300 glow:bg-green-500/100 rounded-full border-2 md:border-4 border-white ${
-                  index % 2 === 0 ? "-right-2 md:-right-3" : "-left-2 md:-left-3"
+                className={`relative w-full md:w-1/2 ${
+                  index % 2 === 0 ? "md:text-right md:pr-8" : "md:text-left md:pl-8"
                 }`}
-              ></div>
+              >
+                <div className="p-4 rounded-lg shadow-md glow:text-green-500/100 text-copy-primary text-sm md:text-base border-2 border-border">
+                  <strong className="block text-base md:text-lg">{portfolio.portfolio_name}</strong>
+
+                  <p className="mt-2 text-xs md:text-sm">{portfolio.portfolio_short_description}</p>
+                  <div className="overflow-hidden my-2">
+                    <img
+                      src="https://picsum.photos/seed/picsum/1920/1080"
+                      alt="Portfolio"
+                      className="w-full h-auto transition-transform duration-500 ease-in-out transform hover:scale-105"
+                    />
+                  </div>
+                  <div className="mb-2">
+                    {portfolio.portfolio_demo && (
+                      <a href={`${portfolio.portfolio_demo}`} className="underline underline-offset-4 mr-4">
+                        Demo
+                      </a>
+                    )}
+                    {portfolio.portfolio_github && (
+                      <a href={`${portfolio.portfolio_github}`} className="underline underline-offset-4 mr-4">
+                        Github
+                      </a>
+                    )}
+                    {portfolio.portfolio_youtube && (
+                      <a href={`${portfolio.portfolio_youtube}`} className="underline underline-offset-4 mr-4">
+                        YouTube
+                      </a>
+                    )}
+                  </div>
+                  <p className="text-copy-primary/35 text-sm">
+                    {portfolio.portfolio_project_type} || {formattedDate}
+                  </p>
+                </div>
+
+                {/* Marker */}
+                <div
+                  className={`hidden md:block absolute top-1/2 transform -translate-y-1/2 w-4 h-4 md:w-6 md:h-6 bg-green-300 glow:bg-green-500/100 rounded-full border-2 md:border-4 border-white ${
+                    index % 2 === 0 ? "-right-2 md:-right-3" : "-left-2 md:-left-3"
+                  }`}
+                ></div>
+              </div>
             </div>
-          </div>
-        );
-      })}
-    </div>
+          );
+        })}
+      </div>
+      {portfoliosShowing.length >= allowedShowing ? (
+        <button
+          className="text-copy-primary border-border border p-2 rounded-2xl m-2"
+          onClick={() => onClick_ShowMore()}
+        >
+          Show More
+        </button>
+      ) : (
+        <></>
+      )}
+    </>
   );
 }
