05cdc51f93d10adc52ced601fe29f9f7e4196925
TianYu-00 <imniyeye@gmail.com>
2024-08-14 21:04:23 +0100

    add video src to portfolios and seed

--- backend/db/data/portfolios.js
+++ backend/db/data/portfolios.js
@@ -5,6 +5,8 @@ module.exports = [
       "An app to allow users to generate suggestions for date night, whether that is with your partner, friend, or as a solo outing.",
     portfolio_start_date: "2024-07-01",
     portfolio_project_type: "Northcoders Project",
+    portfolio_media_src: "",
+    portfolio_media_src_type: "",
     portfolio_youtube: "https://www.youtube.com/watch?v=3c1I65A9X9w",
     portfolio_github: "https://github.com/TianYu-00/WhimsyDate",
     portfolio_demo: "",
@@ -15,6 +17,8 @@ module.exports = [
       "An API to provide programmatic access to application data, simulating a real-world backend service (like Reddit) for frontend architecture.",
     portfolio_start_date: "2024-05-28",
     portfolio_project_type: "Northcoders Project",
+    portfolio_media_src: "",
+    portfolio_media_src_type: "",
     portfolio_youtube: "",
     portfolio_github: "https://github.com/TianYu-00/be-nc-news",
     portfolio_demo: "https://nc-portfolio-1.onrender.com/api",
@@ -24,9 +28,24 @@ module.exports = [
     portfolio_short_description: "A social news aggregation, web content rating and discussion website.",
     portfolio_start_date: "2024-06-17",
     portfolio_project_type: "Northcoders Project",
+    portfolio_media_src:
+      "https://ucb891f73797da46420748caa663.dl.dropboxusercontent.com/cd/0/inline/CYmBzuV7Ta5_c1T3WfMUi-EIHP-8-gEpRHwiMHvMkOSpD4RgdG5ZX0-Z06ylvvSra7y3vWNGt1eFmdGyh3kifW93rJhbZKih7F-FeWjGgbO3wB3hPjOpZoMrRwlL8B_Hy5nEH56I4_ZaDQW6i5iUddoB/file#",
+    portfolio_media_src_type: "video",
     portfolio_youtube: "https://www.youtube.com/watch?v=UvF9yI6z3YE",
     portfolio_github: "https://github.com/TianYu-00/fe-nc-news",
     portfolio_demo: "https://fe-nc-news-tian.netlify.app/",
+  },
+  {
+    portfolio_name: "Palworld Server Manager",
+    portfolio_short_description:
+      "PSM is a simple server-creating & managing tool, offering features like RCON support, Discord webhook, PalWorldSetting edit, configurable auto backup, auto server restart, and restart schedules etc.",
+    portfolio_start_date: "2024-01-25",
+    portfolio_project_type: "Personal Project",
+    portfolio_media_src: "",
+    portfolio_media_src_type: "",
+    portfolio_youtube: "",
+    portfolio_github: "https://github.com/TianYu-00/PalworldServerManager",
+    portfolio_demo: "https://github.com/TianYu-00/PalworldServerManager/releases/latest",
   },
   {
     portfolio_name: "2D Top Down Shooter",
@@ -34,6 +53,9 @@ module.exports = [
       "2D Top down shooter game where the player has the freedom to select their unique abilities. The primary objective is survival, where the player faces successive waves of enemies.",
     portfolio_start_date: "2023-09-21",
     portfolio_project_type: "Personal Project",
+    portfolio_media_src:
+      "https://ucd284dfbe5d00c199fb597d3d58.dl.dropboxusercontent.com/cd/0/inline/CYqgdifOZahtVeLE-L-LmVirP-aFSR7NHO0Ug51VP3NN9YsS0QJT7ym0M0R8me-VRHoRA7Nqev-lRTcwkfC9euJvqKm7drEc8LN9PrsP7nfeOFrgW4_h7xWIAZOq5QAhkhS69U0M9j3aV-iAf2MGEAZo/file#",
+    portfolio_media_src_type: "video",
     portfolio_youtube: "https://www.youtube.com/watch?v=VZXbzsf5cXU",
     portfolio_github: "https://github.com/TianYu-00/Unity-2DBulletHell",
     portfolio_demo: "",
@@ -44,6 +66,9 @@ module.exports = [
       "A small prototype project suggested by AI when asked. The primary objective is to reach the finish point.",
     portfolio_start_date: "2023-03-08",
     portfolio_project_type: "Personal Project",
+    portfolio_media_src:
+      "https://uc41e21c1e5af00c281086579575.dl.dropboxusercontent.com/cd/0/inline/CYryyPzxUCBt4MakxUy55Prpj-BOeYP6iBWdmCIZWcC98yPhSSJHkbo2uiVDKMYtal7ZqASUy6BCilBdgv98lZ0nq9up-WSSMl62jAPE7KY5NaaICTy4fVesJD6ta_qxMU8tZ1qrbVY6zfgrYp9vneNL/file#",
+    portfolio_media_src_type: "video",
     portfolio_youtube: "https://youtu.be/1936MOAyG1g",
     portfolio_github:
       "https://github.com/TianYu-00/Unity-ClonePuzzlePlatformer/tree/7973e7ce93a9f01a1fdc28ecaee82eff9daf6fa0/Clone%20Puzzle%20Platformer",
@@ -55,6 +80,9 @@ module.exports = [
       "This is a demonstration project showcasing the mechanics of walking, aiming, and shooting.",
     portfolio_start_date: "2023-07-03",
     portfolio_project_type: "Personal Project",
+    portfolio_media_src:
+      "https://ucb8f152e2114390b2a6d102438e.dl.dropboxusercontent.com/cd/0/inline/CYpJen28mSHGowq8faS7u3RnFbcMIZdE13XXIPlZXIcV8mporsosLRwU5b6CAXvuus4TAF9KLkpXJcMeIa2iBPojB9zdr_jc_s7BJ9q6XxGVgum-9sKLZFjv_swLKaq1kYizUDmiIHJhrMm4ZBMGIviM/file#",
+    portfolio_media_src_type: "video",
     portfolio_youtube: "https://youtu.be/sYDjvAt8WiA",
     portfolio_github: "",
     portfolio_demo: "",
@@ -65,6 +93,9 @@ module.exports = [
       "Platformer game set in a 2D environment where the player has the freedom to place blocks in place during their movement. The primary objective is to reach the finish point while bypassing obstacles.",
     portfolio_start_date: "2020-11-16",
     portfolio_project_type: "University Project",
+    portfolio_media_src:
+      "https://uc95c6167d37cd51bcd6c3eb7f46.dl.dropboxusercontent.com/cd/0/inline/CYrwv8CkKcRkzz_Qx5NG2GYmDffaA1AXpI66dTJnjn6toFKQ3Q0363qM8dfW17o78RkP2MTlpVt-UJJmx0xZMCo_VkMPEcfF3Vkn1FvvQMvQW6CWgCIJ0c6dcU2dZF-qDcXucjAEqhYAwJvgeOg__lo8/file#",
+    portfolio_media_src_type: "video",
     portfolio_youtube: "https://youtu.be/eNoeY-UQapw",
     portfolio_github:
       "https://github.com/TianYu-00/Unity_GameAWeek/tree/77fe1f8f5b4407798218c718fb4c3ee8180507a4/SubmitGame3%20-%20Light%26Dark/Light%26Dark",
@@ -77,6 +108,9 @@ module.exports = [
       "Platformer game set in a 3D environment where the player has to get to the right color block before time runs out.",
     portfolio_start_date: "2020-08-05",
     portfolio_project_type: "University Project",
+    portfolio_media_src:
+      "https://uc83d9bc893e2a9bafeacdd77ba5.dl.dropboxusercontent.com/cd/0/inline/CYrD4Ryzum0V23OEZeoVTS23Qczvr1c0D2NCzWEfAufKm4X58BRXGGiqayzzOAIjTqNhAVhK2UEiunFW61WoQT6nOpSfgtYhzlu4bBjYWk5b6I1sZVHKPsLY1QM6u9FRkAVqBDdJ3manOukcHyy-1UBG/file#",
+    portfolio_media_src_type: "video",
     portfolio_youtube: "https://youtu.be/RcjLCrKqVBg",
     portfolio_github:
       "https://github.com/TianYu-00/Unity_GameAWeek/tree/77fe1f8f5b4407798218c718fb4c3ee8180507a4/SubmitGame1%20-%20Minigame/Source/Minigame",
@@ -88,6 +122,9 @@ module.exports = [
       "Street fighter type game set in a 2D environment where the player play against another player locally.",
     portfolio_start_date: "2020-11-05",
     portfolio_project_type: "University Project",
+    portfolio_media_src:
+      "https://uca7e52fb5c9d7f8609c18c4075b.dl.dropboxusercontent.com/cd/0/inline/CYpYT5SGzxA6Bqn38vT6_QOVhntZlu-G63Mhl3o5oNPwHkzY-S2iZ5kOzvw_e27Bw70TTUbf0zPO1KLDXRnneTNhjhBBEgqRpZfVLHLGIdnvkNg08wkzl7I58r9jJehSfRYs_bH3poS8mdNcUmFJ9xZg/file#",
+    portfolio_media_src_type: "video",
     portfolio_youtube: "https://youtu.be/1jOz6dIc1NU",
     portfolio_github:
       "https://github.com/TianYu-00/Unity_GameAWeek/tree/a19eacd9de895111eb4b3f0576e51d2a9c95d926/SubmitGame4%20-%20Cooperation%26Competition/Cooperation%26Competition",
@@ -96,9 +133,13 @@ module.exports = [
   },
   {
     portfolio_name: "2D Collectable Card Game",
-    portfolio_short_description: "x",
+    portfolio_short_description:
+      "Single player card game with fixed deck where strategic use of unique card abilities and mana management is crucial.",
     portfolio_start_date: "2022-05-16",
     portfolio_project_type: "University Project",
+    portfolio_media_src:
+      "https://ucaf353ed642657673f49f7eb178.dl.dropboxusercontent.com/cd/0/inline/CYo-RgvZiVmY9iuWYZf85lmTkmTyzKY7umjNeLujr9K1g4PfJOGygp9ilK1MvpEmGW_1z8p1GaHL0OLPvYR2UkZzmloStyBSJOk1uIVxWljeuVrsy1v_ejhlIlT_ndX4o5WEhjCZznrBMlCKKi5yp3Jq/file#",
+    portfolio_media_src_type: "video",
     portfolio_youtube: "https://youtu.be/-KPOI8hnoa0",
     portfolio_github:
       "https://github.com/TianYu-00/Unity-2DCCG/tree/553b6824484f01b35189f400a1afcaaddb82850d/CCG2DSingle",
