a9dd2d65ab27b19fb95cb3d05d35558fbc0fb07b
TianYu-00 <imniyeye@gmail.com>
2024-09-02 21:27:20 +0100

    changes to back to top button icon

--- frontend/src/components/BackToTop.jsx
+++ frontend/src/components/BackToTop.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { ArrowUpCircleIcon } from "@heroicons/react/24/outline";
+import { ArrowUpIcon } from "@heroicons/react/24/outline";
 
 export default function BackToTopButton() {
   const [isBackToTopButtonVisible, setIsBackToTopButtonVisible] = useState(false);
@@ -35,7 +35,7 @@ export default function BackToTopButton() {
           className="fixed bottom-5 right-5 p-1 bg-cta text-white rounded-full shadow-lg hover:bg-cta-active transition-colors z-50"
           aria-label="Back to top button"
         >
-          <ArrowUpCircleIcon className="md:size-10 size-5 text-background" />
+          <ArrowUpIcon className="md:size-7 size-5 text-background" />
         </button>
       )}
     </>
